53e06a42154f0371e7be46cc84187473e24bdb54
Thales Menato <8753631+thamenato@users.noreply.github.com>
2023-09-25 21:50:12 -0400

    feat: add flake support

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "My Dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    alejandra.url = "github:kamadorueda/alejandra/3.0.0";
+    alejandra.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    alejandra,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells.default = pkgs.mkShell {
+        packages = [
+          # tools
+          pkgs.alejandra
+          pkgs.pre-commit
+          # python is required by dotbot
+          pkgs.python311
+          # language server
+          pkgs.yaml-language-server
+        ];
+      };
+    });
+}
