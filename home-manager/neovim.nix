c0cdf422f95abdd9a70902ecb5d841a65b11168f
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-02-26 11:52:23 -0500

    feat: update neovim.nix

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -1,12 +1,9 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   programs.nixvim = {
     enable = true;
 
-    extraPlugins = [ pkgs.vimPlugins.gruvbox ];
-
-    colorscheme = "gruvbox";
+    # theme
+    colorschemes.gruvbox.enable = true;
 
     # vim.g.*
     globals = {};
@@ -19,22 +16,62 @@
       mouse = "a";
     };
 
+    # plugins supported by nixvim
     plugins = {
+      lualine.enable = true;
+      luasnip.enable = true;
+      oil.enable = true;
+      telescope.enable = true;
+      treesitter.enable = true;
+
       lsp = {
         enable = true;
 
         servers = {
+          # javascript/typescript
           tsserver.enable = true;
+          # lua
           lua-ls.enable = true;
+          # terraform
           terraformls.enable = true;
+          # python
           pylsp.enable = true;
         };
       };
 
-      telescope = {
+      nvim-cmp = {
         enable = true;
+        autoEnableSources = true;
+        sources = [
+          {name = "nvim_lsp";}
+          {name = "path";}
+          {name = "buffer";}
+        ];
+
+        mapping = {
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<Tab>" = {
+            action = ''
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif luasnip.expandable() then
+                  luasnip.expand()
+                elseif luasnip.expand_or_jumpable() then
+                  luasnip.expand_or_jump()
+                elseif check_backscape() then
+                  fallback()
+                else
+                  fallback()
+                end
+              end
+            '';
+            modes = ["i" "s"];
+          };
+        };
       };
     };
-  };
-}
 
+    # plugins not yet added to nixvim
+    # extraPlugins = [];
+}
