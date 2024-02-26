54df8c74a8f2f70a260ded5f1df7f9d00cd7534f
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-02-25 22:44:09 -0500

    feat: add nixvim configs

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -5,7 +5,36 @@
     enable = true;
 
     extraPlugins = [ pkgs.vimPlugins.gruvbox ];
+
     colorscheme = "gruvbox";
+
+    # vim.g.*
+    globals = {};
+
+    # vim.o.*
+    options = {
+      number = true;
+      tabstop = 4;
+      shiftwidth = 4;
+      mouse = "a";
+    };
+
+    plugins = {
+      lsp = {
+        enable = true;
+
+        servers = {
+          tsserver.enable = true;
+          lua-ls.enable = true;
+          terraformls.enable = true;
+          pylsp.enable = true;
+        };
+      };
+
+      telescope = {
+        enable = true;
+      };
+    };
   };
 }
 
