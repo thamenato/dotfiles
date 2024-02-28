30cdf967b8cdcd67b2a3ec8089bf6c4bbce63678
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-02-28 18:46:07 -0500

    feat: update home-manager folder

--- flake.nix
+++ flake.nix
@@ -13,65 +13,57 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    nixvim,
-    ...
-  }: let
-    system = "x86_64-linux";
-    user = "thamenato";
-    lib = nixpkgs.lib;
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-  in {
-    devShell."${system}" = pkgs.mkShell {
-      packages = with pkgs; [
-        # tools
-        pre-commit
-        nixpkgs-fmt
-        # language server
-        yaml-language-server
-        nil
-      ];
-    };
-
-    # nixos hosts
-    nixosConfigurations = {
-      kassogtha = lib.nixosSystem {
+  outputs =
+    { self
+    , nixpkgs
+    , home-manager
+    , nixvim
+    , ...
+    }:
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+      pkgs = import nixpkgs {
         inherit system;
-
-        modules = [./hosts/kassogtha/configuration.nix];
+        config.allowUnfree = true;
       };
-      zoth-ommog = lib.nixosSystem {
-        inherit system;
-
-        modules = [./hosts/zoth-ommog/configuration.nix];
-      };
-    };
-
-    # home-manager
-    homeConfigurations = {
-      "${user}@kassogtha" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        modules = [
-          nixvim.homeManagerModules.nixvim
-          ./hosts/kassogtha/home.nix
+    in
+    {
+      devShell."${system}" = pkgs.mkShell {
+        packages = with pkgs; [
+          # tools
+          pre-commit
+          nixpkgs-fmt
+          # language server
+          yaml-language-server
+          nil
         ];
       };
 
-      "${user}@zoth-ommog" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+      # nixos hosts
+      nixosConfigurations = {
+        kassogtha = lib.nixosSystem {
+          inherit system;
 
-        modules = [
-          nixvim.homeManagerModules.nixvim
-          ./hosts/zoth-ommog/home.nix
-        ];
+          modules = [ ./hosts/kassogtha/configuration.nix ];
+        };
+        zoth-ommog = lib.nixosSystem {
+          inherit system;
+
+          modules = [ ./hosts/zoth-ommog/configuration.nix ];
+        };
+      };
+
+      # home-manager
+      homeConfigurations = {
+        thamenato = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          modules = [
+            nixvim.homeManagerModules.nixvim
+            ./home-manager/home.nix
+          ];
+        };
       };
     };
-  };
 }
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -4,38 +4,30 @@
   ...
 }: {
   imports = [
-    ../../home-manager/alacritty.nix
-    ../../home-manager/direnv.nix
-    ../../home-manager/fzf.nix
-    ../../home-manager/git.nix
-    ../../home-manager/hyprland.nix
-    ../../home-manager/kitty.nix
-    ../../home-manager/lazygit.nix
-    ../../home-manager/neovim.nix
-    ../../home-manager/ripgrep.nix
-    ../../home-manager/vscode.nix
-    ../../home-manager/zoxide.nix
-    ../../home-manager/zsh.nix
+    ./apps/alacritty.nix
+    ./apps/direnv.nix
+    ./apps/fzf.nix
+    ./apps/git.nix
+    ./apps/hyprland.nix
+    ./apps/kitty.nix
+    ./apps/lazygit.nix
+    ./apps/neovim.nix
+    ./apps/ripgrep.nix
+    ./apps/vscode.nix
+    ./apps/zoxide.nix
+    ./apps/zsh.nix
   ];
 
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "thamenato";
   home.homeDirectory = "/home/thamenato";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     # terminal
-    direnv
     fastfetch
     httpie
-    fzf
-    macchina
     ranger
-    lazygit
     lazydocker
-    alejandra
+    nixpkgs-fmt
 
     # dependencies
     catppuccin-gtk
@@ -45,7 +37,6 @@
     firefox
 
     # editors/ide
-    helix
     vscode
 
     # apps
@@ -59,6 +50,7 @@
     xivlauncher
     discord
 
+    # fonts
     (nerdfonts.override {
       fonts = [
         "FiraCode"
@@ -79,6 +71,9 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".gnupg/gpg-agent.conf".text = ''
+      pinentry-program /run/current-system/sw/bin/pinentry
+    '';
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
@@ -90,17 +85,6 @@
     #   org.gradle.daemon.idletimeout=3600000
     # '';
   };
-
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/thamenato/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
 
   home.sessionVariables = {
     EDITOR = "nvim";
