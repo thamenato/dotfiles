72c17a6e3f22a5a0b9f0eddab581ee69d5a99dc1
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-03-09 11:48:57 -0500

    chore: comment nvim-cmp

--- home-manager/apps/neovim.nix
+++ home-manager/apps/neovim.nix
@@ -39,37 +39,37 @@
         };
       };
 
-      nvim-cmp = {
-        enable = true;
-        autoEnableSources = true;
-        sources = [
-          { name = "nvim_lsp"; }
-          { name = "path"; }
-          { name = "buffer"; }
-        ];
+      # nvim-cmp = {
+      #   enable = true;
+      #   sources = [
+      #     { name = "nvim_lsp"; }
+      #     { name = "path"; }
+      #     { name = "buffer"; }
+      #   ];
 
-        mapping = {
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
-          "<Tab>" = {
-            action = ''
-              function(fallback)
-                if cmp.visible() then
-                  cmp.select_next_item()
-                elseif luasnip.expandable() then
-                  luasnip.expand()
-                elseif luasnip.expand_or_jumpable() then
-                  luasnip.expand_or_jump()
-                elseif check_backscape() then
-                  fallback()
-                else
-                  fallback()
-                end
-              end
-            '';
-            modes = [ "i" "s" ];
-          };
-        };
-      };
+      #   mapping = {
+      #     "<CR>" = "cmp.mapping.confirm({ select = true })";
+      #     "<Tab>" = {
+      #       action = ''
+      #         function(fallback)
+      #           if cmp.visible() then
+      #             cmp.select_next_item()
+      #           elseif luasnip.expandable() then
+      #             luasnip.expand()
+      #           elseif luasnip.expand_or_jumpable() then
+      #             luasnip.expand_or_jump()
+      #           elseif check_backscape() then
+      #             fallback()
+      #           else
+      #             fallback()
+      #           end
+      #         end
+      #       '';
+      #       modes = [ "i" "s" ];
+      #     };
+      #   };
+      # };
+
     };
   };
 }
