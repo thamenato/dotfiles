9028846bc2cd91fe1a34dee1a3e4ebc3a7da6376
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-04-03 13:09:59 -0400

    feat: update meer7 to use nixos module

--- hosts/meer7/configuration.nix
+++ hosts/meer7/configuration.nix
@@ -5,7 +5,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    ./../../modules/system.nix
+    ./../../nixos
   ];
 
   # Bootloader.
@@ -21,62 +21,9 @@
   boot.initrd.luks.devices."luks-ab1df3b4-57db-440d-b340-b71db1de7e1d".device = "/dev/disk/by-uuid/ab1df3b4-57db-440d-b340-b71db1de7e1d";
   boot.initrd.luks.devices."luks-ab1df3b4-57db-440d-b340-b71db1de7e1d".keyFile = "/crypto_keyfile.bin";
 
+  # Networking
   networking.hostName = "thales-meer7"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Enable networking
   networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/New_York";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager = {
-    gnome.enable = true;
-    xfce.enable = true;
-  };
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "intl";
-  };
-
-  # Configure console keymap
-  console.keyMap = "us-acentos";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.thales = {
@@ -85,10 +32,6 @@
     extraGroups = [ "networkmanager" "wheel" ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  security.polkit.enable = true;
   hardware.opengl.enable = true;
 
   # system packages
@@ -103,17 +46,8 @@
   programs.nix-ld.libraries = with pkgs; [ ];
 
   # docker
-  virtualisation.docker = {
-    enable = true;
-    daemon.settings = {
-      bip = "172.26.0.1/24";
-      dns = [ "1.1.1.1" "8.8.8.8" ];
-      features = {
-        containerd-snapshotter = true;
-        buildkit = true;
-      };
-    };
-  };
+  virtualisation.docker.daemon.settings.bip = "172.26.0.1/24";
+  virtualisation.docker.daemon.settings.dns = [ "1.1.1.1" "8.8.8.8" ];
   users.extraGroups.docker.members = [ "thales" ];
 
   # services
