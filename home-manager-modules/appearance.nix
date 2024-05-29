5d2afebdff123c9ae6b58fb7e037c76772804a71
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-05-28 22:10:14 -0400

    feat: update appearance configs

--- home-manager-modules/appearance.nix
+++ home-manager-modules/appearance.nix
@@ -1,23 +1,33 @@
 { pkgs, ... }:
 {
-  home.pointerCursor = {
-    gtk.enable = true;
-    # x11.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic";
-    size = 16;
+  home = {
+    packages = with pkgs; [
+      # themes
+      flat-remix-gtk
+      flat-remix-gnome
+      flat-remix-icon-theme
+    ];
+    pointerCursor = {
+      gtk.enable = true;
+      # x11.enable = true;
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Classic";
+      size = 16;
+    };
   };
 
   gtk = {
     enable = true;
     theme = {
       package = pkgs.flat-remix-gtk;
-      name = "Flat-Remix-GTK-Grey-Darkest";
+      name = "Flat-Remix-GTK-Grey-Dark";
     };
 
     iconTheme = {
-      package = pkgs.gnome.adwaita-icon-theme;
-      name = "Adwaita";
+      # package = pkgs.gnome.adwaita-icon-theme;
+      # name = "Adwaita";
+      package = pkgs.flat-remix-gnome;
+      name = "Flat-Remix-Violet-Dark";
     };
 
     font = {
