2e3a7d6d351c6d2c2bdacde116889b54bc8a2e95
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-07-05 21:57:00 -0400

    feat: move more config to sway

--- home-manager-modules/appearance/sway.nix
+++ home-manager-modules/appearance/sway.nix
@@ -1,24 +1,50 @@
-{ wayland, ... }:
+{ pkgs, home, wayland, ... }:
+let
+  modifier = "Mod4"; # super key
+  background = ../../misc/backgrounds/143952-abstract_art-color-blue-atmosphere-violet-2560x1440.jpg;
+in
 {
+  # home = {
+  #   file = {
+  #     ".config/sway/background.jpg".source = background;
+  #   };
+  # };
+
   wayland.windowManager.sway = {
     enable = true;
     wrapperFeatures.gtk = true;
 
     config = {
-      modifier = "Mod4"; # super key
+      modifier = modifier;
       terminal = "alacritty";
+
+      # keybindings = { };
 
       startup = [
         { command = "vivaldi"; }
       ];
 
       output = {
-        # "DP-1" = {
-        #   mode = "3840x1600@59.994Hz";
-        # };
-        # "HDMI-A-1" = {
-        #   mode = "1920x1080@60Hz";
-        # };
+        "*" = { bg = "${background} fit"; };
+      };
+      # DP-1 = {
+      #   resolution = "2560x1440";
+      #   position = "0,1080";
+      # };
+
+      input = {
+        "type:keyboard" = {
+          xkb_layout = "us";
+          xkb_variant = "intl";
+          xkb_options = "terminate:ctrl_alt_bksp";
+          xkb_numlock = "enable";
+        };
+
+        "type:touchpad" = {
+          dwt = "enabled";
+          tap = "enabled";
+          natural_scroll = "enabled";
+        };
       };
     };
   };
