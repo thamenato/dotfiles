2d3225d32f0ed07ea34efca7f18113d55d2d490d
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-07-09 17:39:23 -0400

    feat: add catppuccin theme to rofi

--- home-manager-modules/appearance/rofi.nix
+++ home-manager-modules/appearance/rofi.nix
@@ -2,6 +2,9 @@
 {
   programs.rofi = {
     enable = true;
-    theme = "${pkgs.rofi}/share/rofi/themes/glue_pro_blue.rasi";
+    theme = ./catppuccin-frappe.rasi;
+
+    # To select one of the built-in themes:
+    # theme = "${pkgs.rofi}/share/rofi/themes/glue_pro_blue.rasi";
   };
 }
