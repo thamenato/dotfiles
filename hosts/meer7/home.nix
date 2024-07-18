5e4d7a5dd34028573827a24a9205a7359efd3325
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-07-18 11:01:37 -0400

    refactor: small adjustment to home settings in meer7

--- hosts/meer7/home.nix
+++ hosts/meer7/home.nix
@@ -7,24 +7,24 @@
     ../../home-manager-modules
   ];
 
-  home.username = lib.mkForce "thales";
-  home.homeDirectory = lib.mkForce "/home/thales";
+  home = {
+    username = lib.mkForce "thales";
+    homeDirectory = lib.mkForce "/home/thales";
 
-  home.packages = with pkgs; [
-    # dependencies/tools
-    devenv
-    python3
-    pipx
-  ];
+    packages = with pkgs; [
+      # dependencies/tools
+      devenv
+      python3
+      pipx
+    ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    ".config/xxh/config.xxhc".text = ''
-      hosts:
-        ".*":
-          +s: zsh
-    '';
+    file = {
+      ".config/xxh/config.xxhc".text = ''
+        hosts:
+          ".*":
+            +s: zsh
+      '';
+    };
   };
 
   wayland.windowManager.sway =
