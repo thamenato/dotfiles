a2d6c83ece80fc4d5213627d33054a9019dc6dd1
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-08-14 11:06:31 -0400

    feat: change nix to lix to test it

--- nixos-modules/settings/default.nix
+++ nixos-modules/settings/default.nix
@@ -9,7 +9,9 @@
 
   # nix settings
   nix = {
-    package = pkgs.nixVersions.latest;
+    # package = pkgs.nixVersions.latest;
+    # package = pkgs.nixVersions.nix_2_23;
+    package = pkgs.lix;
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
       allowed-users = [ "thamenato" "thales" ];
