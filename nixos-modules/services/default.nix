84d8da7742b948d7ca50373a988f59021f884131
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-10-08 19:49:10 -0400

    feat: back to ly and gnome

--- nixos-modules/services/default.nix
+++ nixos-modules/services/default.nix
@@ -33,28 +33,18 @@
 
   # Enable the windowing system.
   services.displayManager = {
-    sddm = {
+    ly = {
       enable = true;
-      enableHidpi = true;
-      settings = {
-        General = {
-          GreeterEnvironment = "QT_SCREEN_SCALE_FACTORS=2,QT_FONT_DPI=192";
-        };
-      };
     };
-  };
-
-  services.desktopManager = {
-    plasma6.enable = true;
   };
 
   services.xserver = {
     enable = true;
 
     # displayManager.gdm.enable = true;
-    # desktopManager = {
-    #   gnome.enable = true;
-    # };
+    desktopManager = {
+      gnome.enable = true;
+    };
   };
 
   # enable docker
