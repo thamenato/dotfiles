74cdb7d3e918ac170a96799c331132275cb48227
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-12-14 18:27:52 -0500

    feat(services): refactor the file

--- nixos/services.nix
+++ nixos/services.nix
@@ -1,50 +1,47 @@
 { ... }:
 {
-  # Enable pipewire
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
-
-  # mount thumbdrive
-  services.gvfs.enable = true;
-  services.udisks2.enable = true;
-
-  # configure keymap in X11
-  services.xserver.xkb = {
-    variant = "intl";
-    layout = "us";
-  };
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.pcscd.enable = true;
-
-  # Enable the windowing system.
-  services.displayManager = {
-    ly = {
+  services = {
+    pipewire = {
       enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+    # Enable CUPS to print documents.
+    printing.enable = true;
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+
+    # mount thumbdrive
+    gvfs.enable = true;
+    udisks2.enable = true;
+
+    # configure keymap in X11
+    xserver.xkb = {
+      variant = "intl";
+      layout = "us";
+    };
+
+    # Enable the OpenSSH daemon.
+    openssh.enable = true;
+    pcscd.enable = true;
+
+    displayManager = {
+      ly = {
+        enable = true;
+      };
+    };
+    # Enable the windowing system.
+    xserver = {
+      enable = true;
+
+      # displayManager.gdm.enable = true;
+      desktopManager = {
+        gnome.enable = true;
+      };
     };
   };
-
-  services.xserver = {
-    enable = true;
-
-    # displayManager.gdm.enable = true;
-    desktopManager = {
-      gnome.enable = true;
-    };
-  };
-
 }
