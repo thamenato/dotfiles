903b1174be7833b089e7281d3ab1372c8ddf9ff5
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-01-10 15:57:36 -0500

    chore: update flake

--- hosts/kassogtha/hardware-configuration.nix
+++ hosts/kassogtha/hardware-configuration.nix
@@ -22,6 +22,7 @@
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
+  boot.kernelParams = [ "amdgpu.dcdebugmask=0x10" ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
