c9909d87ce224939ce1b8f0393cef73ae1e02116
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-02-12 20:37:30 -0500

    feat: configure bg for zoth-ommog

--- hosts/zoth-ommog/home.nix
+++ hosts/zoth-ommog/home.nix
@@ -1,36 +1,20 @@
-{lib, ...}: {
+{
+  lib,
+  backgrounds,
+  ...
+}: {
   wayland.windowManager = let
     output = "HDMI-A-1";
-    resolution = "3840x2160@120.000Hz";
-    background = ../../misc/backgrounds/wallhaven-d6jzvg_3840x2160.png;
+    resolution = "3840x2160@120";
   in {
-    sway = {
-      config = {
-        workspaceOutputAssign = [
-          {
-            inherit output;
-            workspace = "1";
-          }
-          {
-            inherit output;
-            workspace = "2";
-          }
-        ];
-
-        output = {
-          ${output} = {
-            position = "0,0";
-            bg = "${background} fit";
-            res = resolution;
-            scale = "2";
-          };
-        };
-      };
-    };
     hyprland = {
       settings = {
         monitor = lib.mkForce ["${output},${resolution},auto,2"];
       };
     };
+  };
+  services.hyprpaper.settings = {
+    preload = builtins.attrValues backgrounds;
+    wallpaper = [",${backgrounds."wallhaven-d6jzvg_3840x2160.png"}"];
   };
 }
