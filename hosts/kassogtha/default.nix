cf286cb3a5818cb7301658a0c7339589c03b9284
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-03-08 19:03:25 -0500

    feat: add wallpaper to kassogtha

--- hosts/kassogtha/default.nix
+++ hosts/kassogtha/default.nix
@@ -1,35 +1,24 @@
-{lib, ...}: {
-  home = {
-    # packages = with pkgs; [];
-  };
+{
+  lib,
+  backgrounds,
+  ...
+}: {
+  # home = {
+  #   packages = with pkgs; [];
+  # };
 
   wayland.windowManager = let
     output = "eDP-1";
-    ultrawide_bg = ../../misc/backgrounds/wallhaven-kxo38d_1920x1080.png;
     resolution = "1920x1080@144.00Hz";
   in {
-    sway = {
-      config = {
-        workspaceOutputAssign = [
-          {
-            inherit output;
-            workspace = "1";
-          }
-        ];
-
-        output = {
-          ${output} = {
-            position = "0,0";
-            bg = "${ultrawide_bg} fit";
-            res = resolution;
-          };
-        };
-      };
-    };
     hyprland = {
       settings = {
         monitor = lib.mkForce ["${output},${resolution},auto,1"];
       };
     };
+  };
+  services.hyprpaper.settings = {
+    preload = builtins.attrValues backgrounds;
+    wallpaper = [",${backgrounds."wallhaven-kxo38d_1920x1080.png"}"];
   };
 }
