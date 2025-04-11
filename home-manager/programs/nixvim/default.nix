1b61241c758453f08da3ff9ffb948649280b9032
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-04-11 17:37:25 -0400

    feat: add opts to nixvim

--- home-manager/programs/nixvim/default.nix
+++ home-manager/programs/nixvim/default.nix
@@ -1,3 +1,6 @@
+# Credits:
+#  - loosely copied/inspired by https://github.com/JMartJonesy/kickstart.nixvim
+#
 {
   imports = [
     ./conform.nix
@@ -33,6 +36,33 @@
         xsel.enable = true;
       };
       register = "unnamedplus";
+    };
+
+    opts = {
+      # Show line numbers
+      number = true;
+
+      # Don't show the mode, since it's already in the statusline
+      showmode = false;
+
+      # Save undo history
+      undofile = true;
+
+      # Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+      ignorecase = true;
+      smartcase = true;
+
+      # Keep signcolumn on by default
+      signcolumn = "yes";
+
+      # Decrease mapped sequence wait time
+      # Displays which-key popup sooner
+      timeoutlen = 300;
+
+      # if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+      # instead raise a dialog asking if you wish to save the current file(s)
+      # See `:help 'confirm'`
+      confirm = true;
     };
 
     plugins = {
