0bc17fcd0544c57a6b9304a84b359996891f7828
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-05-05 11:51:37 -0400

    feat: configure monitor

--- hosts/thales-precision-5490/wayland.nix
+++ hosts/thales-precision-5490/wayland.nix
@@ -13,12 +13,14 @@
       config = {
         output = {
           "eDP-1" = {
+            # laptop
             bg = "${backgrounds."wallhaven-kxo38d_1920x1080.png"} fit";
-            pos = "0 0";
+            pos = "5120 0";
           };
-          "DP-4" = {
-            bg = "${backgrounds."wallhaven-kxo38d_1920x1080.png"} center";
-            pos = "1920 0";
+          "DP-2" = {
+            # external
+            bg = "${backgrounds."wallhaven-rrvygj_5120x1440.png"} center";
+            pos = "0 0";
           };
         };
       };
