7689b391e0c098d13cd3d1219f57d93a56c79f0c
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-07-01 17:40:28 -0400

    chore: flake update

--- flake.nix
+++ flake.nix
@@ -5,12 +5,10 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
-      "https://thamenato.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "thamenato.cachix.org-1:mKr904ZtswIxgI9QQtLlDK6tGDLCgRx0syBbrP7Rvss="
     ];
   };
 
