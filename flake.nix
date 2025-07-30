7fbd681e648a77114df762fc4dd562c8121d809e
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-07-29 20:56:40 -0400

    feat: add private nix cache substituter

--- flake.nix
+++ flake.nix
@@ -3,10 +3,12 @@
 
   nixConfig = {
     extra-substituters = [
+      "https://nix-cache.cthyllaxy.xyz"
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
     ];
     extra-trusted-public-keys = [
+      "nix-cache.cthyllaxy.xyz:CEJYeiGUveq4GMALY2GHhcIwrr5PwYwdUj6skoHmBH8="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
