ed83e8fb7b0ceb0596305a62362ea32fd16e43e7
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-10-17 23:16:49 -0400

    feat: add fonts to stylix

--- home-manager/gtk.nix
+++ home-manager/gtk.nix
@@ -7,20 +7,10 @@
     # note to self: do not use `gtk.cursorTheme`
     #
 
-    # theme = {
-    #   package = pkgs.sweet;
-    #   name = "Sweet-Dark";
-    # };
-
     iconTheme = {
       package = pkgs.candy-icons;
       name = "candy-icons";
     };
-
-    # font = {
-    #   name = "Iosevka NF";
-    #   size = 11;
-    # };
 
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = true;
--- home-manager/stylix.nix
+++ home-manager/stylix.nix
@@ -3,6 +3,28 @@
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
 
+    fonts = with pkgs; {
+      serif = {
+        package = nerd-fonts.iosevka;
+        name = "Iosevka NF";
+      };
+
+      sansSerif = {
+        package = nerd-fonts.iosevka;
+        name = "Iosevka NF";
+      };
+
+      monospace = {
+        package = nerd-fonts.iosevka;
+        name = "Iosevka NFM";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
+
     targets = {
       zen-browser.profileNames = ["default"];
       hyprlock.enable = false;
