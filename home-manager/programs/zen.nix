51b5a0671329e61318ebebb11624610b6a244a33
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-10-18 22:40:34 -0400

    feat: add config to zen-browser

--- home-manager/programs/zen.nix
+++ home-manager/programs/zen.nix
@@ -1,6 +1,35 @@
 {
-  programs.zen-browser = {
+  programs.zen-browser = let
+    # extensions: https://github.com/0xc000022070/zen-browser-flake?tab=readme-ov-file#extensions
+    mkExtensionSettings = builtins.mapAttrs (_: pluginId: {
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/${pluginId}/latest.xpi";
+      installation_mode = "force_installed";
+    });
+  in {
     enable = true;
+
+    policies = {
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true; # save webs for later reading
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+
+      ExtensionSettings = mkExtensionSettings {
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = "bitwarden-password-manager";
+      };
+    };
+
     profiles."default" = {
       containersForce = true;
       containers = {
