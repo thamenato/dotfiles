680a7bb01186e4266e2fcba288d5e298b9d94203
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-10-19 20:34:09 -0400

    feat: add monitor config for zoth-ommog

--- hosts/zoth-ommog/default.nix
+++ hosts/zoth-ommog/default.nix
@@ -2,5 +2,15 @@
   services.hyprpaper.settings = {
     preload = builtins.attrValues backgrounds;
     wallpaper = [",${backgrounds."wallhaven-d6jzvg_3840x2160.png"}"];
+    programs.niri.settings.outputs = {
+      "HDMI-A-1" = {
+        mode = {
+          width = 4096;
+          height = 2160;
+          refresh = 120.000;
+        };
+        scale = 2;
+      };
+    };
   };
 }
