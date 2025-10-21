b90c039b558353af6bd70008ce3d7ef85558d858
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-10-21 19:01:58 -0400

    chore: flake update

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -3,31 +3,33 @@
     enable = true;
     lfs.enable = true;
 
-    userName = "Thales Menato";
-    userEmail = "8753631+thamenato@users.noreply.github.com";
+    settings = {
+      user = {
+        name = "Thales Menato";
+        email = "8753631+thamenato@users.noreply.github.com";
+      };
 
-    extraConfig = {
-      push.autoSetupRemote = true;
       fetch.prune = true;
       init.defaultBranch = "main";
-      pull.rebase = true;
-      rerere.enabled = true;
       merge.conflictStyle = "diff3";
+      pull.rebase = true;
+      push.autoSetupRemote = true;
+      rerere.enabled = true;
+
+      alias = {
+        pu = "push";
+        co = "checkout";
+        cm = "commit";
+        st = "status";
+        sw = "switch";
+        root = "rev-parse --show-toplevel";
+      };
     };
 
     signing = {
       format = "ssh";
       key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGkoP+Uu+fdknJf1b4J6ydudNe4FeTqMpDxBmvJiTkzM";
       signByDefault = true;
-    };
-
-    aliases = {
-      pu = "push";
-      co = "checkout";
-      cm = "commit";
-      st = "status";
-      sw = "switch";
-      root = "rev-parse --show-toplevel";
     };
   };
 }
