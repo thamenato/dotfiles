c78b70bbaa224e3e1cc07765666035816f33be5a
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-10-21 22:59:57 -0400

    feat: move cursor config to stylix

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{
   imports = [
     ./dconf.nix
     ./gtk.nix
@@ -19,28 +19,12 @@
       man = "batman";
     };
 
-    file = {
-      # ".config/niri/config.kdl".source = ./config/niri/config.kdl;
-    };
-
     sessionVariables = {
       EDITOR = "nvim";
       NH_HOME_FLAKE = "$HOME/dotfiles";
       ELECTRON_OZONE_PLATFORM_HINT = "wayland";
       OZONE_PLATFORM = "wayland";
       GDK_BACKEND = "wayland";
-    };
-
-    pointerCursor = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Classic";
-      size = 24;
-
-      gtk.enable = true;
-      hyprcursor = {
-        enable = true;
-        size = 24;
-      };
     };
   };
 
--- home-manager/stylix.nix
+++ home-manager/stylix.nix
@@ -6,10 +6,11 @@
     base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
     polarity = "dark";
 
-    # cursor = {
-    #   package = pkgs.bibata-cursors;
-    #   name = "Bibata-Modern-Ice";
-    # };
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 24;
+    };
 
     fonts = with pkgs; {
       serif = {
@@ -37,6 +38,47 @@
       waybar.enable = true;
       rofi.enable = true;
       mako.enable = true;
+      gtk = {
+        enable = false;
+        extraCss = ''
+          /*
+          from comment: https://github.com/ghostty-org/ghostty/discussions/3190#discussioncomment-11690311
+          debug: env GTK_DEBUG=interactive ghostty
+          https://docs.gtk.org/gtk4/css-overview.html
+          https://docs.gtk.org/gtk4/css-properties.html
+          */
+
+          headerbar {
+            margin: 0;
+            padding: 0;
+            min-height: 20px;
+          }
+
+          tabbar tabbox {
+            margin: 0;
+            padding: 0;
+            min-height: 10px;
+            background-color: #1a1a1a;
+            font-family: monospace;
+          }
+
+          tabbar tabbox tab {
+            margin: 0;
+            padding: 0;
+            color: #9ca3af;
+            border-right: 1px solid #374151;
+          }
+
+          tabbar tabbox tab:selected {
+            background-color: #2d2d2d;
+            color: #ffffff;
+          }
+
+          tabbar tabbox tab label {
+            font-size: 13px;
+          }
+        '';
+      };
     };
   };
 }
