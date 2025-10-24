8352b7209192e8848de0e0731e83c26490d7ec1a
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-10-24 11:42:37 -0400

    fix: nixgl wrap apps that had some issues

--- hosts/thales-precision-5490/packages.nix
+++ hosts/thales-precision-5490/packages.nix
@@ -11,11 +11,10 @@
     nh
     pwvucontrol
     (config.lib.nixGL.wrap satty)
-    signal-desktop-bin
-    # slack
+    (config.lib.nixGL.wrapOffload signal-desktop-bin)
+    (config.lib.nixGL.wrapOffload slack)
     slurp
-    # spotify
-    sway-contrib.grimshot
+    spotify
     uv
     xfce.thunar
     (config.lib.nixGL.wrap zoom-us)
