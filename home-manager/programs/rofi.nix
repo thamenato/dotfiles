4f8a7178205128ee602ad599d79ed82651bfd134
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-11-03 22:21:12 -0500

    feat: start ricing rofi

--- home-manager/programs/rofi.nix
+++ home-manager/programs/rofi.nix
@@ -1,6 +1,25 @@
-{
+{config, ...}: {
   programs.rofi = {
     enable = true;
-    theme = {};
+
+    theme = let
+      inherit (config.lib.formats.rasi) mkLiteral;
+    in {
+      # https://man.archlinux.org/man/rofi-theme.5
+
+      "entry" = {
+        placeholder = "Search...";
+      };
+
+      "element" = {
+        orientation = mkLiteral "horizontal";
+        children = map mkLiteral ["element-icon" "element-text"];
+        spacing = mkLiteral "15px";
+      };
+
+      "element-icon" = {
+        size = mkLiteral "1.5em";
+      };
+    };
   };
 }
