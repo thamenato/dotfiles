e04f09c1eef6b33bd08033d636e14be78ae1e22a
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-11-12 19:06:42 -0500

    refactor: adjust nixpkgs url input

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
