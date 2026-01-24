2a9f23005a641b18d26354ddc1107a6c75719699
Thales Menato <8753631+thamenato@users.noreply.github.com>
2026-01-24 18:59:14 -0500

    feat: adjust bg for kassogtha

--- hosts/kassogtha/default.nix
+++ hosts/kassogtha/default.nix
@@ -1,16 +1,10 @@
-{
-  utils,
-  backgrounds,
-  ...
-}: let
-  background = utils.mkSwaybg [
-    {
-      output = "eDP-1";
-      image = "${backgrounds."wallhaven-kxo38d_1920x1080.png"}";
-    }
-  ];
-in {
-  programs.niri.settings.spawn-at-startup = [
-    background
-  ];
+{backgrounds, ...}: {
+  home.file.".cache/noctalia/wallpapers.json" = {
+    text = builtins.toJSON {
+      defaultWallpaper = "${backgrounds."wallhaven-kxo38d_1920x1080.png"}";
+      wallpapers = {
+        "eDP-1" = "${backgrounds."wallhaven-kxo38d_1920x1080.png"}";
+      };
+    };
+  };
 }
