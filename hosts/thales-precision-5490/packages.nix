2b2dc026a2ead87bb2f6d17101bd288a82bfe02e
Thales Menato <8753631+thamenato@users.noreply.github.com>
2026-03-09 21:27:49 -0400

    feat: add flox to work laptop

--- hosts/thales-precision-5490/packages.nix
+++ hosts/thales-precision-5490/packages.nix
@@ -1,20 +1,27 @@
-{pkgs, ...}:
-with pkgs; {
-  home.packages = [
-    _1password-cli
-    _1password-gui
-    devbox
-    image-roll
-    # jiratui
-    pwvucontrol
-    satty
-    signal-desktop
-    slack
-    slurp
-    spotify
-    uv
-    thunar
-    xwayland-satellite
-    zoom-us
-  ];
-}
+{
+  inputs,
+  pkgs,
+  ...
+}: let
+  flox = inputs.flox.packages.x86_64-linux.default;
+in
+  with pkgs; {
+    home.packages = [
+      # jiratui
+      _1password-cli
+      _1password-gui
+      devbox
+      flox
+      image-roll
+      pwvucontrol
+      satty
+      signal-desktop
+      slack
+      slurp
+      spotify
+      thunar
+      uv
+      xwayland-satellite
+      zoom-us
+    ];
+  }
