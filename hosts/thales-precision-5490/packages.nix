ef2de0163e3f2d4c54af5d19a6eefcf47e70d24a
Thales Menato <8753631+thamenato@users.noreply.github.com>
2026-05-13 10:14:56 -0300

    chore: flake update

--- hosts/thales-precision-5490/packages.nix
+++ hosts/thales-precision-5490/packages.nix
@@ -1,27 +1,20 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: let
-  flox = inputs.flox.packages.x86_64-linux.default;
-in
-  with pkgs; {
-    home.packages = [
-      # jiratui
-      _1password-cli
-      _1password-gui
-      devbox
-      flox
-      image-roll
-      pwvucontrol
-      satty
-      signal-desktop
-      slack
-      slurp
-      spotify
-      thunar
-      uv
-      xwayland-satellite
-      zoom-us
-    ];
-  }
+{pkgs, ...}:
+with pkgs; {
+  home.packages = [
+    # jiratui
+    _1password-cli
+    _1password-gui
+    devbox
+    image-roll
+    pwvucontrol
+    satty
+    signal-desktop
+    slack
+    slurp
+    spotify
+    thunar
+    uv
+    xwayland-satellite
+    zoom-us
+  ];
+}
